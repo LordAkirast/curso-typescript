8d8a569e5af851db34e283e6d4c96b1392a5b894
LordAkirast <darkwarish@gmail.com>
2023-09-26 21:03:23 -0300

    Teste simples funcionando. Tudo ocorreu certo.

---
+++ tests/exercicio-aula03/tests/app.test.ts
@@ -0,0 +1,9 @@
+import calculator from "calculator"
+
+describe("testes de matematica", () => {
+    it("deve retornar o resultado da soma de dois valores", () => {
+        const resultado = calculator.sum(1, 2)
+
+        expect(resultado).toEqual(4)
+    })
+})
