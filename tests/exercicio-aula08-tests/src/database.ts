0424448e369c0d468d709a3f8d075bfdea506e3c
LordAkirast <darkwarish@gmail.com>
2023-10-02 23:08:51 -0300

    tem que usar o npm i dotenv, configurar o dotenv, colocar ele no database.ts, criar a base de dados manualmente com as colunas também.

--- tests/exercicio-aula08-tests/src/database.ts
+++ tests/exercicio-aula08-tests/src/database.ts
@@ -1,4 +1,9 @@
 import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv';
+
+// Carregue as variáveis de ambiente do arquivo .env para testes
+dotenv.config();
+
 const prisma = new PrismaClient();
 
 export default prisma;
